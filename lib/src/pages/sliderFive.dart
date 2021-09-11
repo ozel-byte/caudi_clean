fb1c3eec0c53d8a27957387dedad07e9346ffce3
201241 <77318315+201241@users.noreply.github.com>
2021-09-11 17:34:09 -0500

    vista 5 y 6

--- lib/src/pages/sliderFive.dart
+++ lib/src/pages/sliderFive.dart
@@ -11,10 +11,147 @@ class SLiderFive extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Text(
-            "Hola paaaaa",
-            textAlign: TextAlign.center,
-          )
+          Image(
+            image: AssetImage(
+                "assets/reshot-illustration-reading-books-ZDLTUEQ9WJ.png"),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.only(left: 20, top: 12),
+                child: Text(
+                  "¡RECUERDA ANTES DEL TRATAMIENTO!",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.only(left: 20, top: 12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Verifica los límites permisibles para  descargas de",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12,
+                          color: Color(0xff7e7b7b)),
+                    ),
+                    Text(
+                      "aguas residuales en la",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12,
+                          color: Color(0xff7e7b7b)),
+                    ),
+                    Text(
+                      "NOM-001-SEMARNAT-2021.",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12,
+                          color: Color(0xff7e7b7b)),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.only(left: 20, top: 12),
+                child: Text(
+                  "Algunos límites permisibles en promedio mensual.",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.only(left: 20, top: 12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "- Temperatura: 35° C",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                          color: Color(0xff7e7b7b)),
+                    ),
+                    Text(
+                      "- Grasas y Aceites: 15 mg/L",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                          color: Color(0xff7e7b7b)),
+                    ),
+                    Text(
+                      "- Sólidos Suspendidos totales: 60 mg/L",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                          color: Color(0xff7e7b7b)),
+                    ),
+                    Text(
+                      "- Demanda Química de Oxígeno (DQO): 150 mg/L",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                          color: Color(0xff7e7b7b)),
+                    ),
+                    Text(
+                      "- pH: 6-9",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                          color: Color(0xff7e7b7b)),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+          Column(children: [
+            Container(
+                padding: EdgeInsets.only(top: 35),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                        onPressed: () {
+                          Navigator.popAndPushNamed(context, "sliderSix");
+                        },
+                        style: ButtonStyle(
+                            padding: MaterialStateProperty.all(
+                              EdgeInsets.only(
+                                  left: 50, right: 50, top: 5, bottom: 5),
+                            ),
+                            backgroundColor:
+                                MaterialStateProperty.all(Color(0xff3dbfe5))),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Siguiente",
+                              style: TextStyle(
+                                  fontSize: 19, color: Color(0xffffffff)),
+                            )
+                          ],
+                        ))
+                  ],
+                )),
+          ])
+          //reshot-illustration-reading-books-ZDLTUEQ9WJ.png
         ],
       ),
     );
--- lib/src/pages/sliderSix.dart
+++ lib/src/pages/sliderSix.dart
@@ -5,8 +5,69 @@ class SliderSix extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Text(
+                  "¡Llegó la hora del tratamiento!",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                      color: Color(0xff22ADCC)),
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.only(top: 12),
+                child: Text(
+                  "¡Por un agua sin residuos!",
+                  style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 22,
+                      color: Color(0xff22ADCC)),
+                ),
+              )
+            ],
+          ),
+          Column(children: [
+            Container(
+                padding: EdgeInsets.only(top: 35),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                            padding: MaterialStateProperty.all(
+                              EdgeInsets.only(
+                                  left: 80, right: 80, top: 10, bottom: 10),
+                            ),
+                            backgroundColor:
+                                MaterialStateProperty.all(Color(0xff3dbfe5))),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Jugar",
+                              style: TextStyle(
+                                  fontSize: 20, color: Color(0xffffffff)),
+                            )
+                          ],
+                        ))
+                  ],
+                )),
+          ])
+        ],
+      ),
     );
   }
 }
