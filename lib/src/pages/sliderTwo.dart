cf8a04493b3998d4545e9f504cdcf31c84540bf2
Kevin O <Kevinnuricumbo@gmail.com>
2021-09-11 14:55:43 -0500

    Elegir asesor

--- lib/src/pages/sliderTwo.dart
+++ lib/src/pages/sliderTwo.dart
@@ -1,12 +1,87 @@
 import 'package:flutter/material.dart';
 
-class SliderTwo extends StatelessWidget {
-  const SliderTwo({Key? key}) : super(key: key);
+/*Container(
+            margin: EdgeInsets.symmetric(vertical: 60),
+            child: Column(
+              children: [
+                Text(
+                  'Elige un Asesor:',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                )
+              ],
+            ),
+          ),*/
 
+class SliderTwo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 60, horizontal: 10),
+                child: Text(
+                  'Elige un Asesor:',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                ),
+              ),
+            ],
+          ),
+          asesor(size),
+        ],
+      ),
+    );
+  }
+
+  Widget asesor(Size size) {
     return Container(
-      child: null,
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                child: Column(
+                  children: [
+                    Text(
+                      'Ingrese un nombre de usuario',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                child: Column(
+                  children: [
+                    Text(
+                      "Aqui va imagen",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Text('Jugar')],
+          )
+        ],
+      ),
     );
   }
 }
